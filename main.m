d96d8e17ecfa01a5ae1fccaafa5d2a84c7c4c06a
ziming wang <w952001632@163.com>
2022-02-18 15:51:44 +0800

    'commit'

--- main.m
+++ main.m
@@ -1,3 +1,7 @@
 fitFunc = 'objFunc'; % name of objective/fitness function
 T = 200; % total number of evaluations
-[bestSoFarFit, bestSoFarSolution] = simpleEA(fitFunc, T);
+input.pop_size = 4;
+input.min = 0;
+input.max = 31;
+input.gen_length = 5;
+[bestSoFarFit, bestSoFarSolution] = simpleEA(fitFunc, T,input);
--- simpleEA.m
+++ simpleEA.m
@@ -29,23 +29,83 @@ fitness_pop=[];% record the best fitness in current population
 %% Below starting your code
 
 % Initialise a population
-%% TODO
-
+population = randi([input.min,input.max],input.pop_size,1);
+genotypes = dec2bin(population);
 
 % Evaluate the initial population
 %% TODO
+fitness = objFunc(population)
 
+[m,idx] = max(fitness);
+fitness_pop = [fitness_pop,m]
+if m > bestSoFarFit
+    bestSoFarFit = m;
+    bestSoFarSolution = population(idx);
+end
+
+nbGen = nbGen + 1;
+nbEval = nbEval + input.pop_size;
+[fitness_gen,bestSoFarFit];
+fitness_gen=[fitness_gen,bestSoFarFit];
+solution_gen=[solution_gen,bestSoFarSolution];
 % Start the loop
 while (nbEval<T) 
 % Reproduction (selection, crossver)
 %% TODO
-
+crossover_prob = fitness./sum(fitness);
+offspring = [];
+for i = 1:input.pop_size/2
+    parent = [];
+    for j = 1:2
+        r = rand();
+        for idx = 1:input.pop_size
+            if r>sum(crossover_prob(1:idx-1)) && r<=sum(crossover_prob(1:idx))
+                break;
+            end
+        end
+        parent = [parent, idx];
+    end
+    crossover_idx = randi(input.gen_length-1);
+    offspring = [offspring; [genotypes(parent(1),1:crossover_idx), genotypes(parent(2),crossover_idx+1:end)]];
+    offspring = [offspring; [genotypes(parent(2),1:crossover_idx), genotypes(parent(1),crossover_idx+1:end)]];
+end
 % Mutation
 %% TODO
+mutation_prob = 0.1;
+for i = 1:input.pop_size
+    isMutation = rand(1,input.gen_length)<mutation_prob;
+    offspring(i,isMutation) = dec2bin('1'-offspring(i,isMutation))';
+end
 
+genotypes = offspring;
+population = bin2dec(genotypes);
+fitness = objective(population);
+[m,idx] = max(fitness);
+fitness_pop = [fitness_pop,m]
+if m > bestSoFarFit
+    bestSoFarFit = m;
+    bestSoFarSolution = population(idx);
+end
 
+nbGen = nbGen + 1;
+nbEval = nbEval + input.pop_size;
+
+fitness_gen=[fitness_gen,bestSoFarFit];
+solution_gen=[solution_gen,bestSoFarSolution];
+
+end
 bestSoFarFit
 bestSoFarSolution
+figure,plot(1:nbGen,fitness_gen,'black')
+title('fitnessGen')
+figure,plot(1:nbGen,solution_gen,'blue') 
+title('solutionGen')
+figure,plot(1:nbGen,fitness_pop,'red') 
+title('fitnessGop')
+
+end
+
+
 
 
 
